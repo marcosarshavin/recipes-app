8bef66d4457b4ad1a5f2acab9b0cf69cc61f50aa
marcos <marcosrivas17@gmail.com>
2017-04-15 12:30:00 +0200

    Add Service to retrieve data from Mock Constant

--- recipes-front/src/main/recipes-a2/src/app/app.component.ts
+++ recipes-front/src/main/recipes-a2/src/app/app.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
-import { RECETAS } from './mock_data/Recetas';
+import { Component, OnInit } from '@angular/core';
+import { Receta } from './model/Receta';
+import { RecipesService } from './services/recipes.service'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [RecipesService]
 })
 export class AppComponent {
-  recetas = RECETAS;
+  recetas: Receta[];
+
+  constructor(private recipeService: RecipesService) {
+
+  }
+
+  ngOnInit() {
+    this.getRecipes();
+  }
+
+  getRecipes() {
+    this.recipeService.findAllRecipes().then(
+      recetas => this.recetas = recetas
+    );
+  }
 }
--- recipes-front/src/main/recipes-a2/src/app/services/recipes.service.ts
+++ recipes-front/src/main/recipes-a2/src/app/services/recipes.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Receta } from '../model/Receta'; 
+//Mock Data:
+import { RECETAS } from '../mock_data/RECETAS'; 
 
 @Injectable()
 export class RecipesService {
@@ -7,7 +10,14 @@ export class RecipesService {
 
   constructor() { }
 
-  findAllRecipes()  {
-    //Not yet implemented
+  /*
+    Note on promises:
+    When using a remote server, users don't have to wait for the server to respond; 
+    additionally, you aren't able to block the UI during the wait.
+
+    A Promise essentially promises to call back when the results are ready
+  */
+  findAllRecipes(): Promise<Receta[]>  {
+    return Promise.resolve(RECETAS);
   }
 }
